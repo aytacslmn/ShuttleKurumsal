5a8e98212f03e26b5478f7680ceddfb49f7adc14
savashan0019 <savasciftci1905@gmail.com>
2016-01-25 17:38:23 +0200

    1/25/2016 17:37 İletişim sayfası yapıldı

--- app/views/Template_FrontEnd/iletisim_view.php
+++ app/views/Template_FrontEnd/iletisim_view.php
@@ -3,66 +3,67 @@
 <link href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/iletisim.css" rel="stylesheet" type="text/css"/>
 <div class="container">
     <div class="row">
-        <div class="col-md-6">
+       
+        <div class="col-md-12"style="padding-top: 30px">
+            <div>
+                <div class="panel panel-default">
+                    <div class="text-center header">Ofis Adresimiz</div>
+                    <div class="panel-body text-center">
+                       
+                        <div>
+                            Erciyes Üniversitesi<br />
+                            Tekno Park 5. Bina 1. Kat<br />
+                            05374807900<br />
+                            info@shuttleoperator.com<br />
+                        </div>
+                        <hr />
+                        <div style="border:2px"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.1063965277444!2d35.52978031531304!3d38.715366165258395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzjCsDQyJzU1LjMiTiAzNcKwMzEnNTUuMSJF!5e0!3m2!1str!2str!4v1453723183717" width="1100" height="529" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 "></div>
+        <div class="col-md-6"style="padding-top: 10px;padding-bottom: 20px">
             <div class="well well-sm">
                 <form class="form-horizontal" method="post">
                     <fieldset>
-                        <legend class="text-center header">Contact us</legend>
+                        <legend class="text-center header">İletişim</legend>
                         <div class="form-group">
                             <div class="col-md-10 col-md-offset-1">
-                                <input id="fname" name="name" type="text" placeholder="First Name" class="form-control">
+                                <input id="fname" name="name" type="text" placeholder="Ad" class="form-control">
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="col-md-10 col-md-offset-1">
-                                <input id="lname" name="name" type="text" placeholder="Last Name" class="form-control">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-10 col-md-offset-1">
-                                <input id="email" name="email" type="text" placeholder="Email Address" class="form-control">
+                                <input id="lname" name="name" type="text" placeholder="Soyad" class="form-control">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-10 col-md-offset-1">
-                                <input id="phone" name="phone" type="text" placeholder="Phone" class="form-control">
+                                <input id="email" name="email" type="text" placeholder="Email" class="form-control">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-10 col-md-offset-1">
-                                <textarea class="form-control" id="message" name="message" placeholder="Enter your massage for us here. We will get back to you within 2 business days." rows="7"></textarea>
+                                <input id="phone" name="phone" type="text" placeholder="Telefon" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-1">
+                                <textarea class="form-control" id="message" name="message" placeholder="Mesajınızı Yazınız" rows="7"></textarea>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-12 text-center">
-                                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                                <button type="submit" class="btn btn-primary btn-lg">Gönder</button>
                             </div>
                         </div>
                     </fieldset>
                 </form>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div>
-                <div class="panel panel-default">
-                    <div class="text-center header">Our Office</div>
-                    <div class="panel-body text-center">
-                        <h4>Address</h4>
-                        <div>
-                            2217 Washington Blvd<br />
-                            Washington DC<br />
-                            #(703) 1234 1234<br />
-                            service@company.com<br />
-                        </div>
-                        <hr />
-                        <div id="map1" class="map2">
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
